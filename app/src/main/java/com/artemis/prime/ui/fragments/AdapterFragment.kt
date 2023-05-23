305fb0bb5f72d16f1b6063ed33a8da3b26b6c826
elmomoloco7890 <thegreek.god@gmail.com>
2023-05-23 12:10:11 -0700

    Eliminated the not used imports

--- app/src/main/java/com/artemis/prime/ui/fragments/AdapterFragment.kt
+++ app/src/main/java/com/artemis/prime/ui/fragments/AdapterFragment.kt
@@ -5,11 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
-import com.artemis.prime.R
 import com.artemis.prime.adapter.FlamingoHolidayAdapter
 import com.artemis.prime.data.FlamingoHolidaysItem
 import com.artemis.prime.databinding.FragmentAdapterBinding
